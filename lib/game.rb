e27d0a13ec69fecf43d53982ca1380a551b32f93
Rob Bikmansurov <robb@mail.ru>
2020-07-27 13:28:54 +0500

    working variant, but not correct with ёй if they not present in the word

---
+++ lib/game.rb
@@ -0,0 +1,88 @@
+# Game Hangman
+class Game
+  attr_reader :attempts
+
+  def initialize(word = nil)
+    @word = word || from_file
+    @attempts = 0
+    @state = 0
+    @wrong_letters = []
+    @correct_letters = []
+    @correct_letters << @word[0]
+    @correct_letters << @word[-1]
+  end
+
+  def description
+    <<~TEXT
+      Ваша задача - угадать слово, загаданное компьютером.
+      В каждой попытке вы пробуете угадать одну букву.
+      Если буква есть в слове, она открывается.
+      Если нет - рисуется новый элементт виселицы.
+      Успеете отгадать слово до того, как человечка повесят -
+      Вы выиграли. Не успеете - выиграл компьютер.
+    TEXT
+  end
+
+  def finished?
+    @state > 5 || guessed?
+  end
+
+  def guessed?
+    (normalized(word).chars - @correct_letters).size.zero?
+  end
+
+  def from_file
+    file_path = File.join(File.dirname(__FILE__), '..', 'data', 'words.txt')
+    words = File.readlines(file_path, chomp: true)
+    words.sample.upcase
+  end
+
+  def errors
+    @wrong_letters
+  end
+
+  def estimate(letter)
+    @attempts += 1
+    normalized_letter = normalized(letter)
+    normalized_word = normalized(@word)
+    if normalized_word.chars.include?(normalized_letter)
+      @correct_letters << normalized_letter unless @correct_letters.include?(normalized_letter)
+    else
+      @wrong_letters << letter unless @wrong_letters.include?(letter)
+      @state += 1
+    end
+  end
+
+  def result
+    if guessed?
+      "\nВы угадали слово #{@word} за #{@attempts} попыток и заслуженно победили!"
+    else
+      result = field
+      if @wrong_letters.size.positive?
+        result << "\nВы пробовали (#{@wrong_letters.size} букв): #{@wrong_letters.join(', ')}"
+      end
+      result << "\nВы проиграли! Было загадано слово #{@word}"
+    end
+  end
+
+  def word
+    result = ''
+    @word.chars do |letter|
+      result << if @correct_letters.include?(normalized(letter))
+                  letter.upcase
+                else
+                  '-'
+                end
+    end
+    result
+  end
+
+  def field
+    file_path = File.join(File.dirname(__FILE__), '..', 'data', "#{@state}.txt")
+    File.readlines(file_path, chomp: true)
+  end
+
+  def normalized(letters)
+    letters.tr('ЁЙ', 'ЕИ')
+  end
+end
---
+++ main.rb
@@ -0,0 +1,26 @@
+# Задача 22-5 — Виселица в github
+#
+require_relative 'lib/game'
+
+LETTERS = ('А'..'Я').map(&:to_s) << 'Ё'
+
+puts '* * * Игра Виселица * * *'
+
+game = Game.new # ('СВОЙСТВО')
+puts game.description
+
+loop do
+  puts
+  puts "Загадано слово: #{game.word}"
+  puts game.field
+  puts "Вы вводили (#{game.errors.size} букв):  #{game.errors.join(', ')}" if game.errors.size.positive?
+  print 'Введите букву: '
+
+  until LETTERS.include?(letter = STDIN.gets.chomp.upcase)
+    puts 'Нужно ввести русскую букву: '
+  end
+  game.estimate(letter)
+  break if game.finished?
+end
+
+puts game.result
